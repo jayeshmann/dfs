a2d1ed02a1b569e4965d727002922a5965d80437
Jayesh Mann <jayeshmann06@gmail.com>
2021-07-13 23:19:33 +0530

    better gcd

--- 1a.c
+++ 1a.c
@@ -7,32 +7,16 @@ int main()
 
   int a, b;
 
-  printf("Let's calculate GCD:-\n\nEnter first number: ");
+  printf("Let's calculate GCD=>\n\nEnter first number: ");
   scanf("%d", &a);
   printf("Enter second number: ");
   scanf("%d", &b);
 
-  printf("GCD : %d", gcd(a, b));
+  printf("GCD: %d", gcd(a, b));
   return 0;
 }
 
 int gcd(int a, int b)
 {
-  int r, temp;
-
-  if (a < b)
-  {
-    temp = a;
-    a = b;
-    b = temp;
-  }
-
-  while (b != 0)
-  {
-    r = a % b;
-    a = b;
-    b = r;
-  }
-
-  return a;
+  return b ? gcd(b, a % b) : a;
 }
